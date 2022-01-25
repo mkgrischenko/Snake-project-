e9b7f204b614f4ae670c8db6f8bad6587b414096
mkgrischenko <mkgrischenko@edu.hse.ru>
2022-01-25 23:42:28 +0300

    rename

--- Snake_test.py
+++ Snake_test.py
@@ -4,9 +4,15 @@ import unittest
 
 class Snaketests(unittest.TestCase):
 
-    def test_1start(self):
-        self.assertIsNotNone(self.Snake)
+@patch('pygame.ket.get_pressed')
+    def test_exit_Snake(self,test_patch):
+
+        test_patch.return.value = {pygame.K_ESCAPE : True}
+        with self.assertRaises(SystemExit) as exit:
+            Snake.maim()
+        self.assertEqual(exit.exception.code, 1)
         
+
 
 
 
